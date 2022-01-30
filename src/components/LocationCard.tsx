40793ead21878c23587b30c33fe80dbea77d2af9
Zivsteve <zivsteve@gmail.com>
2022-01-30 07:53:17 -0500

    Add place link

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -1,15 +1,29 @@
 import { Box, Image, Stack, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
-const LocationCard = ({ data, large }: { data: any; large?: boolean }) => {
+const LocationCard = ({ data, large, onClick }: { data: any; large?: boolean; onClick?: () => void }) => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <Stack w={large || expanded ? "250px" : "145px"} h={large || expanded ? "80px" : "70px"} shadow="xl" bg="#fff" rounded="xl" p={3} spacing={2} transition="0.3s ease-in-out" onClick={() => setExpanded(!expanded)}>
+    <Stack
+      w={large || expanded ? "250px" : "145px"}
+      h={large || expanded ? "80px" : "70px"}
+      shadow="xl"
+      bg="#fff"
+      rounded="xl"
+      p={3}
+      spacing={2}
+      transition="0.3s ease-in-out"
+      onClick={() => {
+        setExpanded(!expanded);
+        onClick?.();
+      }}
+      cursor={!!onClick ? "pointer" : undefined}
+    >
       <Stack direction="row" spacing={2}>
         <Stack w="145px" spacing={2}>
           <Stack direction="row" align="center" spacing={3}>
-            <Image w={30} h={30} src="https://www.concordia.ca/content/concordia/en/social/guidelines-conduct.img.png/1631757076138.png" />
+            <Image w={30} h={30} src={data.iconData} />
             <Text fontFamily="Poppins" fontSize={13} lineHeight={1}>
               {data?.name || "--"}
             </Text>
@@ -23,7 +37,10 @@ const LocationCard = ({ data, large }: { data: any; large?: boolean }) => {
             </Text>
           </Stack>
         </Stack>
-        <Box></Box>
+        <Stack direction="row" opacity={expanded ? 1 : 0} transition="300ms ease-in-out">
+          <Image src="https://cdn3.iconfinder.com/data/icons/capsocial-round/500/facebook-512.png" w={25} h={25} />
+          <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" w={25} h={25} />
+        </Stack>
       </Stack>
     </Stack>
   );
